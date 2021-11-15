d9ed895d6ce7bb954984e876c20955d61ca18587
Daniel Pak <danielpak1@gmail.com>
2021-11-14 22:22:46 -0800

    explore completed

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Daniel Pak
 
 1) Within a GitHub action that runs whenever code is pushed. This is to automate the process of testing and create testing checkpoints at every push.
 2) No
+3) No, we would not use a unit test to test the "message" feature because it is too large and complicated to test as one unit. It would be better to use end to end testing for such a feature that depends on end users to function.
+4) Yes, we would use unit testing to test the "max message length" since the length checking is a small and independent function. 
+   
+![test results](/test_result.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,70 @@
 // unit.test.js
 
+const {expect} = require('@jest/globals');
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('Checking phone number', () => {
+    expect(functions.isPhoneNumber('621-213-3218')).toBe(true)
+});
+test('Checking phone number', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true)
+});
+test('Checking phone number', () => {
+    expect(functions.isPhoneNumber('anyeonghaseyo')).toBe(false)
+});
+test('Checking phone number', () => {
+    expect(functions.isPhoneNumber('owaowa')).toBe(false)
+});
+
+test('Checking email', () => {
+    expect(functions.isEmail('dapak@ucsd.edu')).toBe(true)
+});
+test('Checking email', () => {
+    expect(functions.isEmail('test@gmail.com')).toBe(true)
+});
+test('Checking email', () => {
+    expect(functions.isEmail('123-456-7890')).toBe(false)
+});
+test('Checking email', () => {
+    expect(functions.isEmail('omonah')).toBe(false)
+});
+
+test('Checking password', () => {
+    expect(functions.isStrongPassword('NotPassword')).toBe(true)
+});
+test('Checking password', () => {
+    expect(functions.isStrongPassword('O482nc')).toBe(true)
+});
+test('Checking password', () => {
+    expect(functions.isStrongPassword('supercalifragilisticexpedialidocious')).toBe(false)
+});
+test('Checking password', () => {
+    expect(functions.isStrongPassword('1234')).toBe(false)
+});
+
+test('Checking date', () => {
+    expect(functions.isDate('1/12/2012')).toBe(true)
+});
+test('Checking date', () => {
+    expect(functions.isDate('01/01/1970')).toBe(true)
+});
+test('Checking date', () => {
+    expect(functions.isDate('6/6/6')).toBe(false)
+});
+test('Checking date', () => {
+    expect(functions.isDate('7/7/7/7/7/7/7')).toBe(false)
+});
+
+test('Checking color', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true)
+});
+test('Checking color', () => {
+    expect(functions.isHexColor('#DD0044')).toBe(true)
+});
+test('Checking color', () => {
+    expect(functions.isHexColor('$FFFFFF')).toBe(false)
+});
+test('Checking color', () => {
+    expect(functions.isHexColor('blue')).toBe(false)
+});
